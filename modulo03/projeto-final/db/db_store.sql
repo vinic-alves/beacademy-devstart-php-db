119c628de086cd07d4e12d6acf34d739099c73fd
= <=>
2022-06-12 22:30:25 -0300

    Tabelas Criadas

--- modulo03/projeto-final/db/db_store.sql
+++ modulo03/projeto-final/db/db_store.sql
@@ -8,3 +8,26 @@ CREATE TABLE tb_category (
     name VARCHAR(30) NOT NULL,
     description VARCHAR (100) NOT NULL
 );
+
+CREATE TABLE tb_product (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    photo VARCHAR(255) NOT NULL,
+    valor FLOAT(5,2) NOT NULL,
+    categoria_id INT(11) NOT NULL,
+    quantity INT(5) NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+
+
+
+
+INSERT INTO tb_category (name, description)
+VALUES
+('informática', 'Produtos de informática e acessórios para computador'),
+('Escritório', 'Canetas, Cadernos, Folhas, etc'),
+('Eletrônicos', 'Tvs, Som Portátil, caixas de Som etc');
+
+
